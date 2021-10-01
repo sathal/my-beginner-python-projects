880a8346cf1eb21962f4de125e2c6ae5b15eed0e
sathal <sathal622@gmail.com>
2021-10-01 07:51:00 -0500

    Including changes made using Pythonista on phone

--- 1-MadLibGenerator/madLibGenerator.py
+++ 1-MadLibGenerator/madLibGenerator.py
@@ -1,36 +1,41 @@
-
-# To run in Atom use Ctrl + i
-
 print('Project 1: Mad Lib Generator')
 
-# The madlib story
-madlib = '''
-Be {3} by this {4} poem:
+# Function to get input from the user
+def getUserInput(wordType):
+    return input('Give me a ' + wordType + ': ')
+
+# method to denote main execution
+def main():
+				# The madlib poem
+				madlib = '''
+Now be {3} with this {4} poem:
 How much {0} could a {1} {2}
 If a {1} could {2} {0}?
 As much {0} as a {1} could {2},
 If a {1} could {2} {0}.
 '''
 
-# Word type list
-wordTypeList = ['noun (plural)','noun (singular)','verb (Example: \'run\')','reaction (Example: \'surprised\')','adjective']
+				# Word type list
+				wordTypeList = ['noun (plural)','noun (singular)','verb (Example: \'run\')','reaction (Example: \'surprised\')','adjective']
 
-# List to hold the user's inputs
-chosenWords = []
+				# List to hold the user's inputs
+				chosenWords = []
 
-# Function to get input from the user
-def getUserInput(wordType):
-    return raw_input('Give me a ' + wordType + ': ')
 
-# List index for use in the wordList
-listIndex = 0
+				# List index for use in the wordList
+				listIndex = 0
 
-# Iterate through the wordList and get user input
-for wordType in wordTypeList:
-    chosenWords.append(getUserInput(wordType))
-    listIndex+=1
+				# Iterate through the wordList and get user input
+				for wordType in wordTypeList:
+				    chosenWords.append(getUserInput(wordType))
+				    listIndex+=1
 
-print(chosenWords)
+				print('\nYour choice of words is excellent: ')
+				print(chosenWords)
 
-# Display the madlib to the console
-print(madlib.format(*chosenWords))
+				# Display the madlib to the console
+				# Must "unpack" the List using '*'
+				print(madlib.format(*chosenWords))
+
+
+main()
