1fa46c7e40b40ea1d55c05cd1226d26c208c3a14
sathal <sathal622@gmail.com>
2021-10-16 14:05:09 -0500

    Adjusting indents in first project file. Changes to python projects README file

--- 1-MadLibGenerator/madLibGenerator.py
+++ 1-MadLibGenerator/madLibGenerator.py
@@ -6,8 +6,8 @@ def getUserInput(wordType):
 
 # method to denote main execution
 def main():
-				# The madlib poem
-				madlib = '''
+    # The madlib poem
+    madlib = '''
 Now be {3} with this {4} poem:
 How much {0} could a {1} {2}
 If a {1} could {2} {0}?
@@ -15,27 +15,26 @@ As much {0} as a {1} could {2},
 If a {1} could {2} {0}.
 '''
 
-				# Word type list
-				wordTypeList = ['noun (plural)','noun (singular)','verb (Example: \'run\')','reaction (Example: \'surprised\')','adjective']
+    # Word type list
+    wordTypeList = ['noun (plural)','noun (singular)','verb (Example: \'run\')','reaction (Example: \'surprised\')','adjective']
 
-				# List to hold the user's inputs
-				chosenWords = []
+    # List to hold the user's inputs
+    chosenWords = []
 
 
-				# List index for use in the wordList
-				listIndex = 0
+	# List index for use in the wordList
+    listIndex = 0
 
-				# Iterate through the wordList and get user input
-				for wordType in wordTypeList:
-				    chosenWords.append(getUserInput(wordType))
-				    listIndex+=1
+	# Iterate through the wordList and get user input
+    for wordType in wordTypeList:
+        chosenWords.append(getUserInput(wordType))
+        listIndex+=1
 
-				print('\nYour choice of words is excellent: ')
-				print(chosenWords)
+    print('\nYour choice of words is excellent: ')
+    print(chosenWords)
 
-				# Display the madlib to the console
-				# Must "unpack" the List using '*'
-				print(madlib.format(*chosenWords))
-
+    # Display the madlib to the console
+    # Must "unpack" the List using '*'
+    print(madlib.format(*chosenWords))
 
 main()
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Project ideas list taken from following site: https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/
+# Sathal's Python Projects Repository
+
+## Purpose and Motivation
+I come from a mainly C Programming Language background and have previous classroom experience with Java through OOP courses. I am interested in learning and gaining familiarity with the Python Programming Language now. This repository mainly serves as a place for me to store my projects as I explore the language :)
+
+
+So far all of the mini projects I've completed come from the following site:
+https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/
+
+### Running list of concepts I've been able to try in Python so far (in no particular order):
