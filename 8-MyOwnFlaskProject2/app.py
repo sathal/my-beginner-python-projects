8f3fbe3bd063a7b1c329a3afec05ce0605dd65a2
sathal <sathal622@gmail.com>
2021-10-31 07:49:53 -0500

    Comments

--- 8-MyOwnFlaskProject2/app.py
+++ 8-MyOwnFlaskProject2/app.py
@@ -1,9 +1,14 @@
+### Steps to run the application in the browser on port 5000 from terminal - by following these steps you are launching a very simple builtin server
+# 1) In the terminal navigate to the directory containing the .py file there the Flask class is instantiated
+# 2) Start the virtual environment by using the following command: $ . venv/bin/activate
+# 3) Tell flask where to find your application by adding it to the FLASK_APP environment variable using the following command: $ export FLASK_APP=yourAppName
+# 4) Enable all development feature by running the following: $ export FLASK_ENV=development
+# 5) Start your application with the following command: $ flask run
+
 from flask import Flask
 
-### Create an instance of the Flask class. This functions as our WSGI (Web Server Gateway Interface) application
-### https://www.fullstackpython.com/wsgi-servers.html
-app = Flask(__name__)
+app = Flask(__name__) # Create an instance of the Flask class. This functions as our WSGI (Web Server Gateway Interface) application
 
-@app.route("/")
+@app.route("/") # Tells Flask what URL should trigger the following method
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!!!</p>" # The method returns what we want displayed in the user's browser
